277d871d4002304c24b748fac374871d3a5f65d5
Sooyeon Lee <qazxcvbnm322@naver.com>
2025-08-11 12:06:36 +0900

    임시 프로젝트 이미지 삽입

--- js/data.js
+++ js/data.js
@@ -3,66 +3,66 @@
 export const portfolioData = [
 {
 id: "1",
-title: "Aesop",
+title: "Aesop Web Renewal Project",
 image: "../images/Aesop_cover.png",
 category: "(UX UI)",
-description: "A web redesign project that reinterprets Aesop’s brand mood with a modern, trend-driven approach."
+description: "브랜드 아이덴티티를 강화하고 사용자 경험을 개선한 이솝 공식 웹사이트 리뉴얼입니다."
 },
 {
 id: "2",
-title: "Aesop",
-image: "../images/Aesop_cover.png",
+title: "Tamburins Web Renewal Project ",
+image: "../images/Tamburins_cover.png",
 category: "(UX UI)",
-description: "A web redesign project that reinterprets Aesop’s brand mood with a modern, trend-driven approach."
+description: "감각적인 비주얼과 스토리텔링 중심으로 구성한 탬버린즈 웹사이트 개편입니다."
 },
 {
 id: "3",
-title: "Aesop",
-image: "../images/Aesop_cover.png",
+title: "Fan app Loopin App design",
+image: "../images/Loopin_cover.png",
 category: "(UX UI)",
-description: "A web redesign project that reinterprets Aesop’s brand mood with a modern, trend-driven approach."
+description: "팬과 아티스트 간 소통을 강화하는 Loopin 모바일 앱 UI·UX 디자인입니다."
 },
 {
 id: "4",
-title: "Aesop",
-image: "../images/Aesop_cover.png",
+title: "Space Tailor Portfolio Design",
+image: "../images/spacetailor_cover.png",
 category: "(UX UI)",
-description: "A web redesign project that reinterprets Aesop’s brand mood with a modern, trend-driven approach."
+description: "스페이스 테일러의 브랜드 제안서 시각화 및 포트폴리오 디자인입니다."
 },
 {
 id: "5",
-title: "Aesop",
-image: "../images/Aesop_cover.png",
+title: "Daily Subscription Portfolio Design",
+image: "../images/일상구독_cover.png",
 category: "(UX UI)",
-description: "A web redesign project that reinterprets Aesop’s brand mood with a modern, trend-driven approach."
+description: "라이프스타일 구독 서비스를 직관적이고 깔끔하게 구성한 포트폴리오 디자인입니다."
 },
 {
 id: "6",
-title: "Aesop",
-image: "../images/Aesop_cover.png",
+title: "Subway App Renewal Project",
+image: "../images/subway_cover.png",
 category: "(UX UI)",
-description: "A web redesign project that reinterprets Aesop’s brand mood with a modern, trend-driven approach."
+description: "주문 편의성과 프로모션 접근성을 높인 서브웨이 공식 앱 개편입니다."
 },
 {
 id: "7",
-title: "Aesop",
-image: "../images/Aesop_cover.png",
-category: "(UX UI)",
-description: "A web redesign project that reinterprets Aesop’s brand mood with a modern, trend-driven approach."
+title: "Henri Matisse Leaflet Design",
+image: "../images/henri_cover.png",
+category: "(EDITORIAL)",
+description: "앙리 마티스의 컷 아웃 기반 리플렛 디자인 프로젝트입니다."
 },
 {
 id: "8",
-title: "Aesop",
-image: "../images/Aesop_cover.png",
-category: "(UX UI)",
-description: "A web redesign project that reinterprets Aesop’s brand mood with a modern, trend-driven approach."
-},
-{
-id: "9",
-title: "Aesop",
-image: "../images/Aesop_cover.png",
+title: "Arozjewelry Clone coding",
+image: "../images/arozjewelry_cover.png",
 category: "(CLONE CODING)",
-description: "A web redesign project that reinterprets Aesop’s brand mood with a modern, trend-driven approach."
-}
+description: "프랑스 쥬얼리 브랜드 아로제주얼리 클론 코딩 결과물입니다."
+},
+// {
+// id: "9",
+// title: "Aesop",
+// image: "../images/Aesop_cover.png",
+// category: "(CLONE CODING)",
+// description: "A web redesign project that reinterprets Aesop’s brand mood with a modern, trend-driven approach."
+// }
 ];
 
