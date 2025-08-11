e2d0f4f83a21b83d90252296c1d7b0243c7efcd9
Sooyeon Lee <qazxcvbnm322@naver.com>
2025-08-11 12:41:23 +0900

    img test2

--- js/galleryData.js
+++ js/galleryData.js
@@ -2,73 +2,73 @@ export const galleryData = [
 {
 id: "1",
 title: "🎨 Words Left Behind",
-image: "../images/gallery_01.png",
+image: "./images/gallery_01.png",
 description: "졸업 전시회, 익명의 누군가가 남기고 간.."
 },
 {
 id: "2",
 title: "💐 Bloom Overload Day",
-image: "../images/gallery_02.png",
+image: "./images/gallery_02.png",
 description: "한 손으론 다 못 들 만큼 꽃을 받은 날, 마음도 무겁고 행복했다."
 },
 {
 id: "3",
 title: "🍕 Off-Duty for Pizza",
-image: "../images/gallery_03.png",
+image: "./images/gallery_03.png",
 description: "쓰리잡 사이, 오랜만에 시간 내서 즐긴 최애 피자집에서 ㅎ.."
 },
 {
 id: "4",
 title: "☕️ My Six-Month Fuel",
-image: "../images/gallery_04.png",
+image: "./images/gallery_04.png",
 description: "6개월 동안 나를 버티게 해준 건 바크콜 한 잔이었는지도.."
 },
 {
 id: "5",
 title: "💻 Pixels & Patience",
-image: "../images/gallery_05.png",
+image: "./images/gallery_05.png",
 description: "하루의 절반이 포트폴리오 작업으로 채워진,,,"
 },
 {
 id: "6",
 title: "🥇 First & Last Attendance Win",
-image: "../images/gallery_06.png",
+image: "./images/gallery_06.png",
 description: "출석 1등 찍었던 날,,, 처음이자 마지막.."
 },
 {
 id: "7",
 title: "🍩 Sweetest Surprise",
-image: "../images/gallery_07.png",
+image: "./images/gallery_07.png",
 description: "찐빵 같은 친구가 준비해 준 생일 도넛ㅎㅎ 이 날 비와서 무사히 들고왔다.."
 },
 {
 id: "8",
 title: "👨🏻‍💻 Not Goodbye, Just Work",
-image: "../images/gallery_08.png",
+image: "./images/gallery_08.png",
 description: "이별이 아닌.. 잠시 작업하러 가는 길."
 },
 {
 id: "9",
 title: "🔥 1AM Fury Ride",
-image: "../images/gallery_09.png",
+image: "./images/gallery_09.png",
 description: "새벽 공기 가르며 달린, 분노의 라이딩!"
 },
 {
 id: "10",
 title: "🥲 Almost Done...ing",
-image: "../images/gallery_10.png",
+image: "./images/gallery_10.png",
 description: "이제 곧 끝낼 거야… 라고 몇 번째 말했더라."
 },
 {
 id: "11",
 title: "🍲 Shabu Shift Snap",
-image: "../images/gallery_11.png",
+image: "./images/gallery_11.png",
 description: "샤브샤브집 알바 중, 찍은 소소한 한 컷."
 },
 {
 id: "12",
 title: "👊🏻 The Team That Made It",
-image: "../images/gallery_12.png",
+image: "./images/gallery_12.png",
 description: "끝까지 함께 달린, 너무 수고한 우리 조원들!"
 },
 ];
